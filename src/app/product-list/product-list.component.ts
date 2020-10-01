5b6ba060f8f7533ca30afc5a67ea3e28e5d9a14f
Alabi Abayomi <abayomi15@gmail.com>
2020-10-01 02:33:36 +0100

    inject a private ProductService Object

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ export class ProductListComponent implements OnInit {
 
   products: product[];
 
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
