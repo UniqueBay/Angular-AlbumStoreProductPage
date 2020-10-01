8e5ec961b1f8100c4edc0dadc41df1798e56491f
Alabi Abayomi <abayomi15@gmail.com>
2020-10-01 02:57:12 +0100

    Added a type to the get product JSON Response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -23,7 +23,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getProduct() {
-    return this._http.get(this._productsUrl);
+  getProduct(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
