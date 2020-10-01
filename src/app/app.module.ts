a1828849926efe4278273409db7b8b6a0cc29043
Alabi Abayomi <abayomi15@gmail.com>
2020-10-01 04:15:35 +0100

    added routes in the NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent },
+  {path: 'product/:id', component: ProductPageComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
